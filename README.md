0bdb9dd90e358168c6d1f6e7930127f822f661e4
SanujBansal <sanuj1770.cse16@chitkara.edu.in>
2021-02-06 16:36:57 +0530

    Getting started, added readme and project structure.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # moody_diaries
+Moody Diaries is a project to make diary writing and talking to yourselves more interesting. Features:-
+1. Titled Diaries
+2. Filter diaries according to mood to revisit some memories.
+3. Search for text within all your diaries.
+4. Themes and great user experience.
+5. Sounds attached to paragraphs, for example tadaa on tapping a particular paragraph. - not sure
+6. Music while writing diary. - not sure
+7. Share diary page or whole diary book with some person (email id).
+8. Mark favourite part of any page, highlight or make bold.
+9. Share publicly. People can read comment and share your publicly shared diaries.
+Feel Free to add More possible features.
+
 
 ## Project setup
 ```
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre>
+      Moody Diaries is a project to make diary writing and talking to yourselves more interesting. Features:-
+      1. Titled Diaries
+      2. Filter diaries according to mood to revisit some memories.
+      3. Search for text within all your diaries.
+      4. Themes and great user experience.
+      5. Sounds attached to paragraphs, for example tadaa on tapping a particular paragraph. - not sure
+      6. Music while writing diary. - not sure
+      7. Share diary page or whole diary book with some person (email id).
+      8. Mark favourite part of any page, highlight or make bold.
+      9. Share publicly. People can read comment and share your publicly shared diaries.
+      Feel Free to add More possible features.
+
+    </pre>
   </div>
 </template>
+<style scoped lang="scss">
+pre {
+  line-height: 2rem;
+}
+</style>
